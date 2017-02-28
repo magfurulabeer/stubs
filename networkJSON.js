4e13ede9e7ced3c527b0716081ae79d17f625b66
Magfurul Abeer <magfurulabeer@gmail.com>
2017-02-27 21:14:38 -0500

    Update networkJSON.js

--- networkJSON.js
+++ networkJSON.js
@@ -1,18 +1,22 @@
 [
   {
     "name": "Scott",
-    "image": "https://raw.githubusercontent.com/magfurulabeer/stubs/master/scott.jpg"
+    "image": "https://raw.githubusercontent.com/magfurulabeer/stubs/master/scott.jpg",
+    "status": "React Native Developer at Nety"
   },
   {
     "name": "Prateek",
-    "image": "https://raw.githubusercontent.com/magfurulabeer/stubs/master/prateek.jpg"
+    "image": "https://raw.githubusercontent.com/magfurulabeer/stubs/master/prateek.jpg",
+    "status": "Operations Manager at Nety"
   },
   {
     "name": "Himanshu",
-    "image": "https://raw.githubusercontent.com/magfurulabeer/stubs/master/himanshu.jpg"
+    "image": "https://raw.githubusercontent.com/magfurulabeer/stubs/master/himanshu.jpg",
+    "status": "Marketing Lead at Nety"
   },
   {
     "name": "Abeer",
-    "image": "https://raw.githubusercontent.com/magfurulabeer/stubs/master/abeer.jpg"
+    "image": "https://raw.githubusercontent.com/magfurulabeer/stubs/master/abeer.jpg",
+    "status": "React Native Developer at Nety"
   }
 ]
