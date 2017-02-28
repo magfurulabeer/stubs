ed158e32d5bbffd4645e5b0b04c7b45e1fe642e9
magfurulabeer <magfurulabeer@gmail.com>
2017-02-27 20:38:05 -0500

    Fix json

--- networkData.js
+++ networkData.js
@@ -1,18 +1,18 @@
 [
   {
-    name: "Scott",
-    image: "https://github.com/magfurulabeer/stubs/blob/master/scott.jpg"
+    "name": "Scott",
+    "image": "https://github.com/magfurulabeer/stubs/blob/master/scott.jpg"
   },
   {
-    name: "Prateek",
-    image: "https://github.com/magfurulabeer/stubs/blob/master/prateek.jpg"
+    "name": "Prateek",
+    "image": "https://github.com/magfurulabeer/stubs/blob/master/prateek.jpg"
   },
   {
-    name: "Himanshu",
-    image: "https://github.com/magfurulabeer/stubs/blob/master/himanshu.jpg"
+    "name": "Himanshu",
+    "image": "https://github.com/magfurulabeer/stubs/blob/master/himanshu.jpg"
   },
   {
-    name: "Abeer",
-    image: "https://github.com/magfurulabeer/stubs/blob/master/abeer.jpg"
+    "name": "Abeer",
+    "image": "https://github.com/magfurulabeer/stubs/blob/master/abeer.jpg"
   },
 ]
