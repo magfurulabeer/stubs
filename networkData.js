e2437517e1e9159c7a1e643f67bc3be242efd40d
Magfurul Abeer <magfurulabeer@gmail.com>
2017-02-27 20:57:44 -0500

    Update networkData.js

--- networkData.js
+++ networkData.js
@@ -1,1 +1,18 @@
-[]
+[
+  {
+    "name": "Scott",
+    "image": "https://raw.githubusercontent.com/magfurulabeer/stubs/master/scott.jpg"
+  },
+  {
+    "name": "Prateek",
+    "image": "https://raw.githubusercontent.com/magfurulabeer/stubs/master/prateek.jpg"
+  },
+  {
+    "name": "Himanshu",
+    "image": "https://raw.githubusercontent.com/magfurulabeer/stubs/master/himanshu.jpg"
+  },
+  {
+    "name": "Abeer",
+    "image": "https://raw.githubusercontent.com/magfurulabeer/stubs/master/abeer.jpg"
+  },
+]
